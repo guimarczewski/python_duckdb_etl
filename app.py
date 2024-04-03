c8b8ecbe75d3aa34c20900a827c97c42d5b74e6b
guimarczewski <guimarczewski@gmail.com>
2024-04-03 22:15:26 +0000

    streamlit app

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
+from pipeline import pipeline
 
-st.title("Olá")
+st.title('Processador de Arquivos')
+
+if st.button('Processar'):
+    with st.spinner('Processando...'):
+        logs = pipeline()
+        # Exibe os logs no streamlit
+        for log in logs:
+            st.write(log)
